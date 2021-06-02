0e023ebc7b329043537486dabf8e3fac9841b58a
neoglez <neoglez@gmail.com>
2021-06-02 14:12:14 +0200

    format

--- combinadics/combinadics.py
+++ combinadics/combinadics.py
@@ -4,15 +4,21 @@
 Created on Wed May 26 13:33:53 2021
 
 @author: neoglez
-Code is a commented python implementaion of James McCaffrey' concepts of
-1. Generating combinations through generating combination successors.
-2. Ranking.
-and (not explicit in McCaffrey' article)
-3. Unranking.
-https://docs.microsoft.com/en-us/archive/msdn-magazine/2004/july/using-combinations-to-improve-your-software-test-case-generation 
+Combinatorial system of degree k ranking and unranking is a standalone simple
+ (not necessarily the most efficient) module to compute the combinadics of a
+ natural number (a.k.a unranking) and the inverse operation (a.k.a ranking).
+
+    Code is a commented python implementation of James McCaffrey' concepts of
+
+    Generating combinations through generating combination successors.
+    Calculating an arbitrary mathematical combination from a given
+        lexicographical index (unranking) and, not explicit in
+        McCaffrey' article,
+    Given a combination, calculate its lexicographical index (ranking).
+
 Note that in the original article the author claims that the combinations he
-presents are in lexicographic order, which is not true. However, both his
-discussion and code are correct.
+    presents are in lexicographic order, which is not true. However, both his
+    discussion and code are correct.
 """
 
 def Choose(n, k):
